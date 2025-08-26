d24ec0b67e418b54829f0ba135d7caca10c6b676
Islombek <islombekazimov04@gmail.com>
2025-08-26 15:37:53 +0500

    small display navbar improved

--- InventoryManagement/Program.cs
+++ InventoryManagement/Program.cs
@@ -4,76 +4,98 @@ using InventoryManagement.Infrastucture.Data;
 using InventoryManagement.Services.Classes;
 using InventoryManagement.Services.Interfaces;
 using InventoryManagement.Stores;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Events;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-builder.Services.AddDbContext<InventoryManagementDbContext>(opt =>
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+try
 {
-    options.Password.RequireNonAlphanumeric = false;
-    options.Password.RequireUppercase = true;
-})
-    .AddEntityFrameworkStores<InventoryManagementDbContext>()
-    .AddDefaultTokenProviders();
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSignalR();
+    builder.Host.UseSerilog((context, services, configuration) => configuration
+        .MinimumLevel.Debug()
+        .WriteTo.Console()
+        .WriteTo.File("logs/logs_txt", LogEventLevel.Information, rollingInterval: RollingInterval.Day)
+        .WriteTo.File("logs/errors_txt", LogEventLevel.Error, rollingInterval: RollingInterval.Day)
+    );
+    builder.Logging.ClearProviders();
 
-builder.Services.AddScoped<ICustomIdService, CustomIdService>();
-builder.Services.AddScoped<IInventoryService, InventoryService>();
-builder.Services.AddScoped<IItemService, ItemService>();
-builder.Services.AddScoped<INotificationService, NotificationService>();
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<HomeStore>();
-builder.Services.AddScoped<AdminStore>();
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
 
-builder.Services.AddTransient<CustomIdService>();
-builder.Services.AddTransient<UserService>();
+    builder.Services.AddDbContext<InventoryManagementDbContext>(opt =>
+        opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-var app = builder.Build();
+    builder.Services.AddDataProtection()
+        .PersistKeysToFileSystem(new DirectoryInfo(Path.Combine(builder.Environment.ContentRootPath, "DataProtectionKeys")))
+        .SetApplicationName("InventoryManagement");
 
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    var db = services.GetRequiredService<InventoryManagementDbContext>();
-    db.Database.Migrate();
+    builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+    {
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = true;
+    })
+        .AddEntityFrameworkStores<InventoryManagementDbContext>()
+        .AddDefaultTokenProviders();
 
-    var userMgr = services.GetRequiredService<UserManager<AppUser>>();
-    var roleMgr = services.GetRequiredService<RoleManager<IdentityRole>>();
-    await SeedDatabase.EnsureSeedAsync(userMgr, roleMgr, db);
+    builder.Services.AddSignalR();
+
+    builder.Services.AddScoped<ICustomIdService, CustomIdService>();
+    builder.Services.AddScoped<IInventoryService, InventoryService>();
+    builder.Services.AddScoped<IItemService, ItemService>();
+    builder.Services.AddScoped<INotificationService, NotificationService>();
+    builder.Services.AddScoped<IUserService, UserService>();
+    builder.Services.AddScoped<HomeStore>();
+    builder.Services.AddScoped<AdminStore>();
+
+    builder.Services.AddTransient<CustomIdService>();
+    builder.Services.AddTransient<UserService>();
+
+    var app = builder.Build();
+
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        var db = services.GetRequiredService<InventoryManagementDbContext>();
+        db.Database.Migrate();
+
+        var userMgr = services.GetRequiredService<UserManager<AppUser>>();
+        var roleMgr = services.GetRequiredService<RoleManager<IdentityRole>>();
+        await SeedDatabase.EnsureSeedAsync(userMgr, roleMgr, db);
+    }
+
+    Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1JEaF5cXmRCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWXdcdHRUQ2NfVU10W0tWYEk=");
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseStaticFiles();
+    app.UseRouting();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapStaticAssets();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}")
+        .WithStaticAssets();
+
+    app.MapHub<InventoryHub>("/hubs/inventory");
+
+    app.Run();
 }
-
-Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1JEaF5cXmRCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWXdcdHRUQ2NfVU10W0tWYEk=");
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+catch(Exception ex)
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    Log.Error(ex.Message);
 }
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapStaticAssets();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
-app.MapHub<InventoryHub>("/hubs/inventory");
-
-app.Run();
